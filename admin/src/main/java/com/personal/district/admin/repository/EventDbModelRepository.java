ce47cfa8829b9d517df39c909056da701d17ccfb
payasbhrigu <bhrigu.payas10@gmail.com>
2024-09-09 09:45:38 +0530

    Event Service - Get and Add Events

--- admin/src/main/java/com/personal/district/admin/repository/EventDbModelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.personal.district.admin.repository;
-
-import com.personal.district.admin.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EventDbModelRepository extends MongoRepository<Event, String> {
-}
